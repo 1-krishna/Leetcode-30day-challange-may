67c66c9a00df471e77d3e50d0ae8b6f78fd4dc58
Krishna <krishnacaresu@gmail.com>
2020-05-09 11:30:31 +0530

    started week 2 and added check if straight line

---
+++ Week-2/CheckIfStraightLine.java
@@ -0,0 +1,53 @@
+/*
+You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+ 
+
+ 
+
+Example 1:
+
+
+
+Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+Output: true
+Example 2:
+
+
+
+Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+Output: false
+ 
+
+Constraints:
+
+2 <= coordinates.length <= 1000
+coordinates[i].length == 2
+-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+coordinates contains no duplicate point.
+
+ */
+
+class Solution {
+    
+    public float findSlope(int x1, int x2, int y1, int y2){
+        return ((float)y2 - (float)(y1))/((float)x2 - (float)x1);
+    }
+    
+    public boolean checkStraightLine(int[][] coordinates) {
+        
+        if(coordinates.length == 2){
+            return true;
+        }
+        
+        float m = findSlope(coordinates[0][0], coordinates[1][0], coordinates[0][1], coordinates[1][1]);
+        float c = coordinates[0][1] - m*coordinates[0][0];
+        
+        for(int i=3;i<coordinates.length;i++){
+            if((float)coordinates[i][1] != m*coordinates[i][0] + c)
+                return false;
+        }
+        
+        return true;
+    }
+}
